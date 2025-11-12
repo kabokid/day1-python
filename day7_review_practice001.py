3cc48e8bc22551f8571e2e7e8432b1c846c9954e
kabo <kabokidpumpkin@gmail.com>
2025-11-12 23:50:13 +0900

    feat: add mood check program combininig function and conditionals (Day7 review)

--- day7_review_practice001.py
+++ day7_review_practice001.py
@@ -1,1 +1,15 @@
-def
+#-----------------------------
+#ユーザーの気分に合わせて、英語で返す小悪魔ミコト風メッセージを出すプログラム
+#-----------------------------
+def check_mood(mood):
+    if mood == "happy":
+        print("\nyou look too cute when you smile!\n")
+    elif mood == "sad":
+        print("\nAww,don't cry~ you'll ruin your pretty face.\n")
+    elif mood == "angry":
+        print("\nOoh, fiely! I like that passion.\n")
+    else:
+        print("\nMmm, mysterious mood… tell me more?\n")
+
+mood_input = input("How are you feeling today?:")
+check_mood(mood_input)
