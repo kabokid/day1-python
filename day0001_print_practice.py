cf15abb79c7594a8f877fe06ff78a2648e739029
kabo <kabokidpumpkin@gmail.com>
2025-12-01 20:56:05 +0900

    Add Day headers to each practice file

--- day0001_print_practice.py
+++ day0001_print_practice.py
@@ -1,1 +1,2 @@
+# Day0001: print の基礎 practice
 #Today I learned print() in Python. I wrote "Hello World".
--- day0002_numberprint_practice.py
+++ day0002_numberprint_practice.py
@@ -1,3 +1,3 @@
-## Day2
+# Day0002: 数値と print の基礎 practice
 #Today I learned about variables and print().
 #I practiced writing number = 7 and print (number).
